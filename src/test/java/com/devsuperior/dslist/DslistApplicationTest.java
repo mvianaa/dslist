0653cd2f859492488a0268779549effd445f5981
mvianaa <mv.mateusvianabatista@gmail.com>
2025-05-19 21:01:11 -0300

    properties, game, orm

--- src/test/java/com/devsuperior/dslist/DslistApplicationTest.java
+++ src/test/java/com/devsuperior/dslist/DslistApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DslistApplicationTests {
+class DslistApplicationTest {
 
 	@Test
 	void contextLoads() {
